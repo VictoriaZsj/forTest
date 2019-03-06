d4c7911eed9b01042a80000c3895809498943366
zoeZheng <1423736181@qq.com>
2019-03-06 17:26:38 +0800

    柯西里

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,16 +1,6 @@
-import axios from 'axios'
 
-const ERR_NO = 0
-
-export function get(url) {
-    return function(params) {
-        return axios.get(url, {
-            params
-        }).then((res) => {
-            const { errno, data } = res.data
-            if (errno === ERR_NO) {
-                return data
-            }
-        }).catch(() => {})
+export function curry(a) {
+    return function(b) {
+        return a + b;
     }
 }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import {curry} from 'api/helpers'
+
+const curryA= curry(2)
+
+console.log(curryA(3))
 new Vue({
     render: h => h(App)
 }).$mount('#app')
