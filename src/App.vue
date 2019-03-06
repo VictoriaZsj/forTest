90a1fea9b66ee4c1c38b30d628c620083074d45e
zoeZheng <1423736181@qq.com>
2019-03-06 15:19:15 +0800

    空额

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+<script>
+import hello from 'components/HelloWorld'
+export default {
+  name: 'app',
+  data(){
+    return{
+      seller:{}
+    }
+  },
+  components: {
+      hello
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  created(){
+
+  }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  .tab-wrapper
+    position :fixed
+    top:136px
+    left:0
+    right:0
+    bottom:0
+</style>
